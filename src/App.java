b22c794a382307820caf484c181036da24770cd6
GABRIEL  HENRIQUE SANTANA DE SOUZA <santanadesouzagabrielhenriquesantana@gmail.com>
2024-09-19 08:55:26 -0300

    add

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import classes.Cliente;
+import classes.conta;
+import classes.Fachada;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Cliente cliente = new Cliente(nome:"Gabe");
+       conta conta = new conta(conta:"00399-9");
+
+       Fachada fachada = new Fachada();
+       fachada.fazerDeposito(valor:200, cliente, conta);
     }
 }
